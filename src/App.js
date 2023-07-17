6b715cda6aa6a1c3112abccd3d1544de37880f43
Cameron Lee <camdlee18@gmail.com>
2023-07-17 19:10:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
+
+
+function App() {
+  const [people, setPeople] = useState(data);
+
+
+  return (
+    <main>
+      <section className="container">
+        <h3>0 Birthdays today</h3>
+          <List />
+          <button onClick={()=> console.log('you clicked me')}>clear all</button>
+      </section>
+    </main>
+  );
+}
+
+export default App;
